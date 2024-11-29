385f81fe8fb7a07808b78eff8b46c7a65e0ef979
Guilherme Lepsch <lepsch@gmail.com>
2024-11-29 23:26:27 +0000

    Publish bezier_kit project

--- app/componets/Projects.tsx
+++ app/componets/Projects.tsx
@@ -6,8 +6,8 @@ export function Projects() {
   return (
     <div className="Projects">
       <Link to={`https://force.${DOMAIN}`}>FORCE - The Force Project</Link>
-      <Link to="https://pub.dev/packages/dart_lapack">Dart Lapack</Link>
-      <p className="disabled">bezier_kit (TBP)</p>
+      <Link to="https://pub.dev/packages/dart_lapack">Dart LAPACK</Link>
+      <Link to="https://pub.dev/packages/bezier_kit">bezier_kit - Dart package for Bezier curves</Link>
       <p className="disabled">lz4 (TBP)</p>
       <p className="disabled">miniz (TBP)</p>
       <p className="disabled">async_protobuf (TBP)</p>
